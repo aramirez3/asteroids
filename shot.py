2e7db0112818d208c9d9c9196e584508e6188478
Angel Ramirez <angelra@gmail.com>
2024-10-03 21:28:47 -0700

    rate limit on shoot

--- shot.py
+++ shot.py
@@ -9,7 +9,6 @@ class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
         self.velocity = pygame.math.Vector2(0, 0)
-        print('shot initialized')
         
     def draw(self, screen):
         pygame.draw.circle(screen, (0, 0, 255), self.position, self.radius)
